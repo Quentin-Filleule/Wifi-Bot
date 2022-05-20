26e970378894ae1cbbf9670d3993b51a73e9d56c
Quentin-Filleule <65295378+Quentin-Filleule@users.noreply.github.com>
2022-05-20 16:57:16 +0200

    Add files via upload

---
+++ Wifi-Bot/myrobot.h
@@ -0,0 +1,45 @@
+#ifndef MYROBOT_H
+#define MYROBOT_H
+
+#include <QObject>
+#include <QTcpSocket>
+#include <QAbstractSocket>
+#include <QDebug>
+#include <QTimer>
+#include <QMutex>
+#include <QWebEngineView>
+
+class MyRobot : public QObject {
+    Q_OBJECT
+public:
+    explicit MyRobot(QObject *parent = 0);
+    void doConnect();
+    void disConnect();
+    QByteArray DataToSend;
+    QByteArray DataReceived;
+    QMutex Mutex;
+    short Crc16();
+
+signals:
+    void updateUI(const QByteArray Data);
+public slots:
+    void connected();
+    void disconnected();
+    void bytesWritten(qint64 bytes);
+    void readyRead();
+    void MyTimerSlot();
+    //fonctions de deplacement:
+    void gauche();
+    void droite();
+    void avant();
+    void arriere();
+    //fonction pour arreter le robot
+    void stop();
+    void video();
+
+private:
+    QTcpSocket *socket;
+    QTimer *TimerEnvoi;
+};
+
+#endif // MYROBOT_H
